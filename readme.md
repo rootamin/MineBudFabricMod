dee2577695d64cb98d3126c192abdf3e30d2c20c
rootamin <osalimohammadamin@gmail.com>
2023-09-16 20:59:05 +0330

    final v2.0.0 commit for 1.20.1

--- readme.md
+++ readme.md
@@ -1,6 +1,28 @@
-The built mod is inside build/libs/minebud-1.0.0-1.20.1.jar
+In order to use this mod, you have to have the latest version of fabric-api.
+\
+The built mod is inside build/libs/minebud-2.0.0-1.20.1.jar
 
--First beta version of the mod.
-Usages:
-it creates a text file inside a path that you provide inside the code.
-the directory has to be present in order for mod to show or modify a code.
+-stable version: 2.0.0
+<hr>
+
+usage:
+By typing:
+``/generatecode`` in the chat, it would generate a unique code for the player inside ``config/players`` directory. each player has it's own text file with the player name on it. the content of the text file is like: ``playername:code``
+\
+You can view your current code by typing ``/showcode`` in chat. by utilizing ``/generatecode`` again, the mod will generate you a new unique code and save the code under the same file as created before.
+\
+The hashtag shown before the code in the chat, is not saved inside the users files.
+\
+contact me if you have any problems: rootamin@outlook.com
+
+<hr>
+![minebuddymod1](https://rootamin.github.io/minebudmod1.png)
+![minebuddymod2](https://rootamin.github.io/minebudmod2.png)
+![minebuddymod3](https://rootamin.github.io/minebudmod3.png)
+<hr>
+Changelogs:
+```
+-2.0.0: failsafe: a codes.txt file will appear inside config directory. every newly generated code, will get checked whether there are same code as it is inside the codes.txt or not. if they are similar codes, it would generate another code and repeat the same process.(this has not been tested properly)
+-2.0.0: failsafe: the codes are now failsafe and will get saved inside the players folder within config directory.
+-2.0.0: feature: made the users to be able to generate new codes.
+```
--- src/main/java/ir/rootamin/minebud/MineBud.java
+++ src/main/java/ir/rootamin/minebud/MineBud.java
@@ -3,22 +3,44 @@ package ir.rootamin.minebud;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.server.ServerStartCallback;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
 
 import java.io.*;
+import java.nio.file.Path;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MineBud implements ModInitializer {
 	public static final String MOD_ID = "minebud";
+	private Set<String> usedCodes = new HashSet<>();
 
 	@Override
 	public void onInitialize() {
+		// Get the config directory
+		Path configDir = FabricLoader.getInstance().getConfigDir();
+
+		// Load used codes from file
+		File codesFile = new File(configDir.toFile(), "codes.txt");
+		if (codesFile.exists()) {
+			try (BufferedReader reader = new BufferedReader(new FileReader(codesFile))) {
+				String line;
+				while ((line = reader.readLine()) != null) {
+					usedCodes.add(line);
+				}
+			} catch (IOException e) {
+				System.out.println("An error occurred while loading the used codes.");
+				e.printStackTrace();
+			}
+		}
+
 		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
 			ServerPlayerEntity player = handler.player;
 
 			// Check if the player has joined before
-			File playerFile = new File("/home/amin/IdeaProjects/MineBud/run/players/" + player.getName().getString() + ".txt");
+			File playerFile = new File(configDir.toFile(), "players/" + player.getName().getString() + ".txt");
 			if (!playerFile.exists()) {
 				// Generate a unique code for the player
 				String uniqueCode = generateUniqueCode();
@@ -38,14 +60,19 @@ public class MineBud implements ModInitializer {
 				ServerPlayerEntity player = context.getSource().getPlayer();
 
 				// Read the player's code from the file
-				File playerFile = new File("/home/amin/IdeaProjects/MineBud/run/players/" + player.getName().getString() + ".txt");
+				// Check if the player has joined before
+				File playerDir = new File(configDir.toFile(), "players");
+				if (!playerDir.exists()) {
+					playerDir.mkdirs();
+				}
+				File playerFile = new File(configDir.toFile(), "players/" + player.getName().getString());
 				try (BufferedReader reader = new BufferedReader(new FileReader(playerFile))) {
 					String line = reader.readLine();
 					String[] parts = line.split(":");
 					String code = parts[1];
 
 					// Send the code to the player
-					player.sendMessage(Text.of("Your code is: " + code), false);
+					player.sendMessage(Text.of("Your code is: #" + code), false);
 				} catch (IOException e) {
 					System.out.println("An error occurred while reading the player's code.");
 					e.printStackTrace();
@@ -54,14 +81,19 @@ public class MineBud implements ModInitializer {
 				return 1;
 			}));
 
-			server.getCommandManager().getDispatcher().register(CommandManager.literal("resetcode").executes(context -> {
+			server.getCommandManager().getDispatcher().register(CommandManager.literal("generatecode").executes(context -> {
 				ServerPlayerEntity player = context.getSource().getPlayer();
 
 				// Generate a new unique code for the player
 				String uniqueCode = generateUniqueCode();
 
 				// Save the new code to the player's file
-				File playerFile = new File("/home/amin/IdeaProjects/MineBud/run/players/" + player.getName().getString() + ".txt");
+				// Check if the player has joined before
+				File playerDir = new File(configDir.toFile(), "players");
+				if (!playerDir.exists()) {
+					playerDir.mkdirs();
+				}
+				File playerFile = new File(configDir.toFile(), "players/" + player.getName().getString());
 				try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(playerFile)))) {
 					out.println(player.getName().getString() + ":" + uniqueCode);
 				} catch (IOException e) {
@@ -70,7 +102,7 @@ public class MineBud implements ModInitializer {
 				}
 
 				// Send the new code to the player
-				player.sendMessage(Text.of("Your new code is: " + uniqueCode), false);
+				player.sendMessage(Text.of("Your new code is: #" + uniqueCode), false);
 
 				return 1;
 			}));
@@ -78,13 +110,29 @@ public class MineBud implements ModInitializer {
 	}
 
 	private String generateUniqueCode() {
-		// Generate a random alphanumeric string between 6 and 10 characters long
-		int length = (int)(Math.random() * 5) + 6;
-		String allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-		StringBuilder code = new StringBuilder(length);
-		for (int i = 0; i < length; i++) {
-			code.append(allowedChars.charAt((int)(Math.random() * allowedChars.length())));
+		String uniqueCode;
+		do {
+			// Generate a random alphanumeric string between 6 and 10 characters long
+			int length = (int)(Math.random() * 5) + 6;
+			String allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+			StringBuilder code = new StringBuilder(length);
+			for (int i = 0; i < length; i++) {
+				code.append(allowedChars.charAt((int)(Math.random() * allowedChars.length())));
+			}
+			uniqueCode = code.toString();
+		} while (usedCodes.contains(uniqueCode));
+
+		usedCodes.add(uniqueCode);
+
+		// Save the newly generated code to codes.txt
+		File codesFile = new File(FabricLoader.getInstance().getConfigDir().toFile(), "codes.txt");
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(codesFile, true)))) {
+			out.println(uniqueCode);
+		} catch (IOException e) {
+			System.out.println("An error occurred while saving the used codes.");
+			e.printStackTrace();
 		}
-		return code.toString();
+
+		return uniqueCode;
 	}
 }
